cd7b91e7e539cac3b3aeb9fe39101897cc0ff8db
jamesscaggs <james.scaggs@gmail.com>
2020-10-07 19:11:21 -0500

    added material and example button

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
+import { Button } from '@material-ui/core';
 
 const Card = props => {
   const { name } = props.official;
   return (
     <div className='card'>
-      <span>{name}</span>
-      <button onClick={() => props.selectOfficial(name)}> Add Official</button>
+      <span style={{ marginRight: '1rem' }}>{name}</span>
+      <Button variant='contained' onClick={() => props.selectOfficial(name)}>
+        Select
+      </Button>
     </div>
   );
 };
